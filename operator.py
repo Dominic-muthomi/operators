96ce0e3473f782b4a38419e349688f8bb03edbb2
Dominic Muthomi <dominicmwebia@gmail.co>
2021-07-13 16:33:10 +0300

    implementoperators

---
+++ operator.py
@@ -0,0 +1,21 @@
+'''
+calculate your age based on the current year and date of birth
+'''
+currentYear=2021
+yearOfBirth=2000
+age=0
+
+age=currentYear-yearOfBirth
+
+print(age)
+
+'''
+Given the exclusive price of milk and the VAT tax,calculate the inclusive price 
+'''
+exclPrice=45.34
+vatPrice=16/100
+vatAmount=exclPrice*vatPrice
+
+inclPrice=exclPrice+vatAmount
+
+print(inclPrice)
